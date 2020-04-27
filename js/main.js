931f52309df9b268c74f72ad5c35b77eb8984aac
Joonas Lehtoranta <joonas.lehtoranta@metropolia.fi>
2020-04-27 14:30:36 +0300

    Toimiva leaflet kartta

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 'use strict';
 
-const myMap = L.map('map'); //perus leaflet kartta
+const map = L.map('mapid'); //perus leaflet kartta
 
 //Tänne kartan haku nykyisten koordinaattien avulla
 //Ja kartan tileLayerin asettaminen
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+map.setView([51.505, -0.09], 13); //Placeholder koordinaatit
